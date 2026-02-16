c2c4e1e78600813414478c170f42c2db26e97b2a
vccxxoo <sidhubehera705@gmail.com>
2026-02-16 13:49:41 +0530

    Update Firebase configuration and imports

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getFirestore }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+  apiKey: "AIzaSyAWiaQepWKPl0vCZrJ8gd3dSuiLghv9IIw",
+  authDomain: "video-c69df.firebaseapp.com",
+  projectId: "video-c69df",
+  storageBucket: "video-c69df.firebasestorage.app",
+  messagingSenderId: "666194002818",
+  appId: "1:666194002818:web:236309eb31ac5e51d4c59b"
 };
 
 const app = initializeApp(firebaseConfig);
